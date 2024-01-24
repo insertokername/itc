968d7259faab08f23f4bdb357f0ea91c2d3e7975
insertokname <insertokname@gmail.com>
2024-01-24 20:54:23 +0200

    centered logo

--- components/Header.js
+++ components/Header.js
@@ -14,41 +14,46 @@ export default function Header() {
 
   return (
     <header className="w-full sticky-nav">
-      <div className="flex flex-col flex-wrap max-w-5xl p-2.5 mx-auto md:flex-row">
-        <div className="flex flex-row items-center justify-between p-2 md:p-1">
-          <a href="/kys">
-            <img src="logo.png" width={340}/>
-            {/* <a className="mb-4 text-2xl font-medium text-black transition duration-300 hover:text-gray-300 md:mb-0">
-              PLUTONIUM
-            </a> */}
+      <div className="grid grid-cols-1 justify-center max-w-xl p-2.5 mx-auto">{/*md:flex-row*/}
+        <a href="/" >
+          <img src="logo.png" width={250} className="mx-auto" />
+        </a>
+        {/*
+        <button
+          className="px-3 py-1 pb-4 ml-auto text-black outline-none md:hidden"
+          type="button"
+          aria-label="button"
+          onClick={() => setNavbarOpen(!navbarOpen)}
+        > 
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="w-6 h-6"
+          >
+            <line x1="3" y1="6" y2="6" x2="21"></line>
+            <line x1="3" y1="12" y2="12" x2="21"></line>
+            <line x1="3" y1="18" y2="18" x2="21"></line>
+          </svg>
+        </button>
+         
+        <div className="flex flex-row items-center text-center justify-between p-2 md:p-1">
+        <a className="mb-4 text-2xl font-medium text-black transition duration-300 hover:text-gray-300 md:mb-0">
+          <p className="text-center">kys</p>
+            PLUTONIUM
           </a>
-          <button
-            className="px-3 py-1 pb-4 ml-auto text-black outline-none md:hidden"
-            type="button"
-            aria-label="button"
-            onClick={() => setNavbarOpen(!navbarOpen)}
-          > {/* hamburger button */}
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="w-6 h-6"
-            >
-              <line x1="3" y1="6" y2="6" x2="21"></line>
-              <line x1="3" y1="12" y2="12" x2="21"></line>
-              <line x1="3" y1="18" y2="18" x2="21"></line>
-            </svg>
-          </button>
         </div>
+              */}
         <div
           className={
-            "md:flex flex-grow items-center" +
-            (navbarOpen ? " flex" : " hidden")
+            "md:flex flex-col flex-grow items-center" +
+            "flex"
+            //(navbarOpen ? " flex" : " hidden")
           }
         >
           <div className="uppercase text-lg flex flex-wrap items-center justify-center pt-1 pl-2 ml-1 space-x-8 md:space-x-16 md:mx-auto md:pl-14">
@@ -72,6 +77,6 @@ export default function Header() {
           </div>
         </div>
       </div>
-    </header>
+    </header >
   );
 }
