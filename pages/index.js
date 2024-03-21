42ff1ea3f29def9568b6bfca0eb767f272aac509
insertokername <insertokname@gmail.com>
2024-03-21 23:32:11 +0200

    adding "THROUGH OUR LENSE..."

--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
       >
         <div className="text-white max-w-[95%] md:max-w-[80%] mx-auto">
           <div className="xl:ml-[3rem] xl:text-3xl xl:text-left text-xl text-center">
-            <h1 className="xl:text-8xl xl:pt-[30%] pt-[65%] text-4xl font-[700]">SERVICE ABOVE SELF</h1>
+            <h1 className="xl:text-8xl xl:pt-[20%] pt-[65%] text-4xl font-[700]">SERVICE ABOVE SELF</h1>
             <p className="xl:mx-0 xl:max-w-[50%] py-6 mx-auto font-[350]">Trăiește experiențe unice, distrează-te și adu o schimbare în comunitatea locală alături de Interact Oradea!</p>
             <button className="py-[2.6rem] px-[1.8rem] mt-5 bg-sky-900 bg-opacity-60 hover:bg-sky-700 hover:bg-opacity-65 text-white font-bold border border-sky-700 rounded">Check out our projects!</button>
           </div>
@@ -76,62 +76,23 @@ export default function Home() {
               </div> */}
             </div>
           </div>
-          {/* <div className="w-full pb-5 mx-auto text-center md:w-11/12">
-            <h1 className="mb-3 text-5xl font-bold tracking-tight text-gray-900 md:text-6xl">
-              Bringing darkness
-              <br />
-              <span className="text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500">
-                straight to the web.
-              </span>
-            </h1>
-            <p className="max-w-xl pt-5 mx-auto text-lg text-gray-600 md:text-lg">
-              Plutonium is a Next.js template styled with TailwindCSS. It boasts
-              built-in dark-mode support, configured Next-SEO for the best SEO
-              optimizations, and clean, organized, easy-to-edit code.
-            </p>
-            <div className="mt-6 text-center md:ml-6">
-              <a
-                className="inline-flex items-center px-5 py-3 text-sm font-medium text-gray-300 transition duration-300 bg-black rounded hover:bg-gray-800"
-                aria-label="learn more"
-                rel="noreferrer"
-                href="https://github.com/minor/plutonium/"
-              >
-                <span className="flex justify-center">GitHub Link</span>
-              </a>
-              <br className="sm:hidden" />
-              <Link href="404">
-                <a
-                  className="inline-flex items-center px-5 py-3 mt-2 ml-0 text-sm font-medium text-gray-700 transition duration-300 border rounded shadow hover:shadow-md md:ml-2"
-                  aria-label="learn more"
-                >
-                  <span className="flex justify-center">See a Demo</span>
-                </a>
-              </Link>
-            </div>
-          </div> */}
-          <div className="relative w-full py-10 mx-auto text-center md:py-32 md:my-12 md:w-10/12">
-            <div className="relative z-10">
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href="https://unsplash.com/photos/e9TrFZZ72DQ"
-              >
-                <img
-                  className="transition duration-700 shadow-xl rounded-xl ring-1 ring-black ring-opacity-5 hover:transform hover:scale-105"
-                  src="/images/placeholder.webp"
-                  alt="Placeholder Image"
-                />
-              </a>
-            </div>
-            <p className="z-10 my-8 text-sm font-medium text-gray-500">
-              Maybe we&apos;re bringing brightness too?
-            </p>
+        </div>
+      </section>
+
+      <section
+        className="w-full h-screen bg-cover bg-center"
+        style={{
+          backgroundImage: "url(bg-our-lense.jpg)",
+        }}
+      >
+        <div className="text-white max-w-[95%] md:max-w-[80%] mx-auto">
+          <div className="xl:ml-[3rem] xl:text-3xl xl:text-left text-xl text-center">
+            <h1 className="xl:text-8xl xl:pt-[20%] pt-[65%] text-4xl font-[700]">THROUGH OUR LENSE...</h1>
+            <p className="xl:mx-0 xl:max-w-[50%] mt-6 mx-auto font-[350]">...pentru că cele mai frumoase amintiri sunt cele în care am ajutat pe cineva fără să așteptăm nimic în schimb.</p>
+            <p className="xl:mx-0 xl:max-w-[50%] mt-5 mx-auto font-[350]">-Byron Pulsifer</p>
+            <button className="py-[2.6rem] px-[1.8rem] mt-11 bg-sky-900 bg-opacity-60 hover:bg-sky-700 hover:bg-opacity-65 text-white font-bold border border-sky-700 rounded">Check out our Gallery!</button>
           </div>
         </div>
-        {/* <div
-          style={{ backgroundImage: "url(/images/blur.png)" }}
-          className="absolute inset-0 w-full h-full bg-bottom bg-no-repeat bg-cover -z-1"
-        /> */}
       </section>
       {/* <Sponsors />
       <Features />
