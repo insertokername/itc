d73ee543c527500db82afae3feec773b5ebdb990
insertokername <insertokname@gmail.com>
2024-03-26 22:53:11 +0200

    changed from hover to focus on hamburger menu

--- components/Headerold.js
+++ components/Headerold.js
@@ -25,11 +25,11 @@ export default function Headerold() {
                         aria-label="button"
                         onClick={() => setNavbarOpen(!navbarOpen)}
                     >
-                        <div class="relative flex overflow-hidden items-center justify-center rounded-full w-[50px] h-[50px] transform transition-all bg-slate-700 ring-0 ring-gray-300 hover:ring-8 group-hover:ring-4 ring-opacity-30 duration-200 shadow-md">
-                            <div class="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center overflow-hidden group-hover:-translate-x-1.5 group-hover:rotate-180">
-                                <div class="bg-white h-[2px] w-7 transform transition-all duration-300 origin-left group-hover:rotate-[42deg] group-hover:w-2/3 delay-150"></div>
-                                <div class="bg-white h-[2px] w-7 rounded transform transition-all duration-300 group-hover:translate-x-10"></div>
-                                <div class="bg-white h-[2px] w-7 transform transition-all duration-300 origin-left group-hover:-rotate-[42deg] group-hover:w-2/3 delay-150"></div>
+                        <div class="relative flex overflow-hidden items-center justify-center rounded-full w-[50px] h-[50px] transform transition-all bg-slate-700 ring-0 ring-gray-300 focus:ring-8 group-focus:ring-4 ring-opacity-30 duration-200 shadow-md">
+                            <div class="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center overflow-hidden group-focus:-translate-x-1.5 group-focus:rotate-180">
+                                <div class="bg-white h-[2px] w-7 transform transition-all duration-300 origin-left group-focus:rotate-[42deg] group-focus:w-2/3 delay-150"></div>
+                                <div class="bg-white h-[2px] w-7 rounded transform transition-all duration-300 group-focus:translate-x-10"></div>
+                                <div class="bg-white h-[2px] w-7 transform transition-all duration-300 origin-left group-focus:-rotate-[42deg] group-focus:w-2/3 delay-150"></div>
                             </div>
                         </div>
                     </button>
