4c6313be4ede6bd49b339255a184b3ca177d8a15
insertokname <insertokname@gmail.com>
2024-03-26 09:13:26 +0200

    updated 404 page

--- pages/404.js
+++ pages/404.js
@@ -20,10 +20,6 @@ export default function Error() {
           <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
             There was an error finding your page.
           </p>
-          <p className="max-w-sm mb-8 text-md">
-            Plutonium doesn&apos;t do well with generic 404 pages. Hence: this
-            page. If you&apos;re here from clicking demo: it was on purpose!
-          </p>
           <Link href="/">
             <a className="px-4 py-2.5 text-black border rounded-lg shadow transition duration-300 hover:shadow-md">
               Return Home
